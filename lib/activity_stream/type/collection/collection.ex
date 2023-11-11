b12aaa173d52675123fabd9e4d5d5a9d1fae3cd8
shahryarjb <shahryar.tbiz@gmail.com>
2023-11-11 16:00:40 +0330

    Add -  all Properties of Collection - activity stream

---
+++ lib/activity_stream/type/collection/collection.ex
@@ -0,0 +1,45 @@
+defmodule MishkaPub.ActivityStream.Type.Collection do
+  use GuardedStruct
+  alias ActivityStream.Type.Collection.Properties
+
+  # totalItems | current | first | last | items
+  guardedstruct do
+    field(:context, String.t(),
+      derive: "sanitize(tag=strip_tags) validate(not_empty_string, url)",
+      default: "https://www.w3.org/ns/activitystreams"
+    )
+
+    field(:type, String.t(),
+      derive: "sanitize(tag=strip_tags) validate(equal=Object)",
+      default: "Collection"
+    )
+
+    field(:summary, String.t(),
+      derive: "sanitize(tag=strip_tags) validate(not_empty_string, max_len=364, min_len=3)"
+    )
+
+    field(:items, struct(), structs: Properties.Items)
+    field(:item, struct(), structs: Properties.Items)
+
+    field(:totalItems, String.t(),
+      derive: "sanitize(tag=strip_tags) validate(not_empty_string, max_len=364, min_len=3)"
+    )
+
+    conditional_field(:current, any()) do
+      field(:current, struct(), struct: Properties.Current, hint: "currentMap")
+      field(:current, String.t(), hint: "current")
+    end
+
+    conditional_field(:first, any()) do
+      field(:first, struct(), struct: Properties.First, hint: "firstMap")
+      field(:first, String.t(), hint: "first")
+    end
+
+    conditional_field(:last, any()) do
+      field(:last, struct(), struct: Properties.Last, hint: "lastMap")
+      field(:last, String.t(), hint: "last")
+    end
+
+    field(:orderedItems, struct(), structs: Properties.OrderedItems)
+  end
+end
