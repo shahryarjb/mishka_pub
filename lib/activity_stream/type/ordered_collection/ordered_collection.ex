0aced96a74dfd40814f8ead1dcd537ff45a764e9
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2023-12-27 12:39:22 +0330

    add docs for each properties of ordered_collection

--- lib/activity_stream/type/ordered_collection/ordered_collection.ex
+++ lib/activity_stream/type/ordered_collection/ordered_collection.ex
@@ -4,40 +4,79 @@ defmodule MishkaPub.ActivityStream.Type.OrderedCollection do
   alias ActivityStream.Type.Collection.Properties.OrderedItems
   alias ActivityStream.Behaviour
 
+  # URI: https://www.w3.org/ns/activitystreams#OrderedCollection
+  # A subtype of Collection in which members of the logical collection are assumed to always be strictly ordered.
+  # ---------------------------------------------------------------------------------------
+  # Properties:
+  # Inherits all properties from Collection.
+  # context | type | summary | totalItems | current | first | last | orderedItems
+  # ---------------------------------------------------------------------------------------
+  # Extends: Collection
   guardedstruct do
+    # URI: https://www.w3.org/ns/activitystreams#context
+    # Identifies the context within which the object exists or an activity was performed.
+    # The notion of "context" used is intentionally vague. The intended function is to serve
+    # as a means of grouping objects and activities that share a common originating context or purpose.
+    # An example could be all activities relating to a common project or event.
+    # Domain: Object
     field(:context, String.t(),
       derive: "sanitize(tag=strip_tags) validate(not_empty_string, url)",
       default: "https://www.w3.org/ns/activitystreams"
     )
 
+    # URI: @type
+    # Identifies the Object or Link type. Multiple values may be specified.
+    # Domain: Object | Link
     field(:type, String.t(),
       derive: "sanitize(tag=strip_tags) validate(not_empty_string, max_len=80, min_len=3)",
       default: "OrderedCollection"
     )
 
+    # URI: https://www.w3.org/ns/activitystreams#summary
+    # A natural language summarization of the object encoded as HTML.
+    # Multiple language tagged summaries may be provided.
+    # Domain:	Object
     field(:summary, String.t(),
       derive: "sanitize(tag=strip_tags) validate(not_empty_string, max_len=364, min_len=3)"
     )
 
+    # URI: https://www.w3.org/ns/activitystreams#totalItems
+    # A non-negative integer specifying the total number of objects contained by the logical
+    # view of the collection. This number might not reflect the actual number
+    # of items serialized within the Collection object instance.
+    # Domain:	Collection
     field(:totalItems, non_neg_integer(),
       derive: "sanitize(tag=strip_tags) validate(not_empty_string, max_len=364, min_len=3)"
     )
 
+    # URI: https://www.w3.org/ns/activitystreams#current
+    # In a paged Collection, indicates the page that contains the most recently updated member items.
+    # Domain:	Collection
     conditional_field(:current, Behaviour.ssls()) do
       field(:current, struct(), struct: Properties.Current, hint: "currentMap")
       field(:current, String.t(), hint: "current")
     end
 
+    # URI: https://www.w3.org/ns/activitystreams#first
+    # In a paged Collection, indicates the furthest preceeding page of items in the collection.
+    # Domain:	Collection
     conditional_field(:first, Behaviour.ssls()) do
       field(:first, struct(), struct: Properties.First, hint: "firstMap")
       field(:first, String.t(), hint: "first")
     end
 
+    # URI: https://www.w3.org/ns/activitystreams#last
+    # In a paged Collection, indicates the furthest proceeding page of the collection.
+    # Domain:	Collection
     conditional_field(:last, Behaviour.ssls()) do
       field(:last, struct(), struct: Properties.Last, hint: "lastMap")
       field(:last, String.t(), hint: "last")
     end
 
+    # URI: none
+    # Ordered Items
+    # Domain: any
+    # TODO: We should cover a standard properties, because the Ordered Items can be anything no limited
     field(:orderedItems, Behaviour.lst(), structs: OrderedItems)
   end
 end
--- lib/activity_stream/type/ordered_collection/properties/ordered_items.ex
+++ lib/activity_stream/type/ordered_collection/properties/ordered_items.ex
@@ -1,6 +1,14 @@
 defmodule ActivityStream.Type.Collection.Properties.OrderedItems do
   use GuardedStruct
 
+  # URI: none
+  # Ordered Items
+  # ---------------------------------------------------------------------------------------
+  # Properties:
+  # name | type
+  # ---------------------------------------------------------------------------------------
+  # Domain: any
+  # TODO: We should cover a standard properties, because the Ordered Items can be anything no limited
   guardedstruct do
     field(:type, String.t(),
       derive: "sanitize(tag=strip_tags) validate(not_empty_string, max_len=80, min_len=3)",
