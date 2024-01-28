40a346a644d6b49c0ce0c1e18671bd3c0ddce5fb
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-01-28 14:44:19 +0330

    add docs for IntransitiveActivity

--- lib/activity_stream/type/intransitive_activity.ex
+++ lib/activity_stream/type/intransitive_activity.ex
@@ -3,6 +3,37 @@ defmodule MishkaPub.ActivityStream.Type.IntransitiveActivity do
   alias ActivityStream.Type.Activity.Properties
   alias ActivityStream.Behaviour
 
+  @types [
+    "Accept",
+    "Add",
+    "Announce",
+    "Arrive",
+    "Block",
+    "Create",
+    "Delete",
+    "Dislike",
+    "Flag",
+    "Follow",
+    "Ignore",
+    "Invite",
+    "Join",
+    "Leave",
+    "Like",
+    "Listen",
+    "Move",
+    "Offer",
+    "Question",
+    "Reject",
+    "Read",
+    "Remove",
+    "TentativeReject",
+    "TentativeAccept",
+    "Travel",
+    "Undo",
+    "Update",
+    "View"
+  ]
+
   # URI: https://www.w3.org/ns/activitystreams#OrderedCollection
   # Instances of IntransitiveActivity are a subtype of Activity representing
   # intransitive actions. The object property is therefore inappropriate for these activities.
@@ -28,14 +59,25 @@ defmodule MishkaPub.ActivityStream.Type.IntransitiveActivity do
     # Identifies the Object or Link type. Multiple values may be specified.
     # Domain: Object | Link
     field(:type, String.t(),
-      derive: "sanitize(tag=strip_tags) validate(not_empty_string, max_len=80, min_len=3)",
-      default: "Travel"
+      derive: "sanitize(tag=strip_tags) validate(enum=String[#{Enum.join(@types, "::")}])",
+      default: "Create"
     )
 
     # URI: https://www.w3.org/ns/activitystreams#summary
     # A natural language summarization of the object encoded as HTML.
     # Multiple language tagged summaries may be provided.
     # Domain:	Object
+    # Example:
+    # {
+    #   "@context": "https://www.w3.org/ns/activitystreams",
+    #   "summary": "A simple note",
+    #   "type": "Note",
+    #   "content": "This is all there is.",
+    #   "generator": {
+    #     "type": "Application",
+    #     "name": "Exampletron 3000"
+    #   }
+    # }
     field(:summary, String.t(),
       derive: "sanitize(tag=strip_tags) validate(not_empty_string, max_len=364, min_len=3)"
     )
@@ -45,6 +87,32 @@ defmodule MishkaPub.ActivityStream.Type.IntransitiveActivity do
     # Any single activity can have multiple actors. The actor may be specified using an indirect Link.
     # Domain:	Activity
     # Subproperty Of:	attributedTo
+    # Example:
+    # {
+    #   "actor": "http://sally.example.org",
+    #   "object": "http://example.org/foo"
+    # }
+    # ----------------------------------------------------
+    # {
+    #   "actor": {
+    #     "type": "Person",
+    #     "id": "http://sally.example.org",
+    #     "summary": "Sally"
+    #   },
+    #   "object": "http://example.org/foo"
+    # }
+    # ----------------------------------------------------
+    # {
+    #   "actor": [
+    #     "http://joe.example.org",
+    #     {
+    #       "type": "Person",
+    #       "id": "http://sally.example.org",
+    #       "name": "Sally"
+    #     }
+    #   ],
+    #   "object": "http://example.org/foo"
+    # }
     conditional_field(:actor, Behaviour.ssls()) do
       field(:actor, struct(), struct: Properties.Actor, hint: "actorMap")
       field(:actor, Behaviour.lst(), structs: Properties.Actor, hint: "actorList")
@@ -62,6 +130,21 @@ defmodule MishkaPub.ActivityStream.Type.IntransitiveActivity do
     # For instance, in the activity "John added a movie to his wishlist",
     # the target of the activity is John's wishlist. An activity can have more than one target.
     # Domain:	Activity
+    # Example:
+    # {
+    #   "actor": "http://sally.example.org",
+    #   "object": "http://example.org/posts/1",
+    #   "target": "http://john.example.org"
+    # }
+    # ----------------------------------------------------
+    # {
+    #   "actor": "http://sally.example.org",
+    #   "object": "http://example.org/posts/1",
+    #   "target": {
+    #     "type": "Person",
+    #     "name": "John"
+    #   }
+    # }
     conditional_field(:target, Behaviour.ss()) do
       field(:target, struct(), struct: Properties.Target, hint: "targetMap")
 
@@ -76,6 +159,47 @@ defmodule MishkaPub.ActivityStream.Type.IntransitiveActivity do
     # results in the creation of a new resource, the result property can
     # be used to describe that new resource.
     # Domain:	Activity
+    # Example
+    # {
+    #   "@context": "https://www.w3.org/ns/activitystreams",
+    #   "name": "A question about robots",
+    #   "id": "http://polls.example.org/question/1",
+    #   "type": "Question",
+    #   "content": "I'd like to build a robot to feed my cat. Which platform is best?",
+    #   "oneOf": [
+    #     {
+    #       "name": "arduino"
+    #     },
+    #     {
+    #       "name": "raspberry pi"
+    #     }
+    #   ],
+    #   "replies": {
+    #     "type": "Collection",
+    #     "totalItems": 3,
+    #     "items": [
+    #       {
+    #         "attributedTo": "http://sally.example.org",
+    #         "inReplyTo": "http://polls.example.org/question/1",
+    #         "name": "arduino"
+    #       },
+    #       {
+    #         "attributedTo": "http://joe.example.org",
+    #         "inReplyTo": "http://polls.example.org/question/1",
+    #         "name": "arduino"
+    #       },
+    #       {
+    #         "attributedTo": "http://john.example.org",
+    #         "inReplyTo": "http://polls.example.org/question/1",
+    #         "name": "raspberry pi"
+    #       }
+    #     ]
+    #   },
+    #   "result": {
+    #     "type": "Note",
+    #     "content": "Users are favoriting &quot;arduino&quot; by a 33% margin."
+    #   }
+    # }
     field(:result, struct(), struct: Properties.Result)
 
     # URI: https://www.w3.org/ns/activitystreams#origin
@@ -84,11 +208,42 @@ defmodule MishkaPub.ActivityStream.Type.IntransitiveActivity do
     # For instance, in the activity "John moved an item to List B from List A",
     # the origin of the activity is "List A".
     # Domain:	Activity
+    # Example:
+    # {
+    #   "@context": "https://www.w3.org/ns/activitystreams",
+    #   "summary": "Sally moved the sales figures from Folder A to Folder B",
+    #   "type": "Move",
+    #   "actor": "http://sally.example.org",
+    #   "object": {
+    #     "type": "Document",
+    #     "name": "sales figures"
+    #   },
+    #   "origin": {
+    #     "type": "Collection",
+    #     "name": "Folder A"
+    #   },
+    #   "target": {
+    #     "type": "Collection",
+    #     "name": "Folder B"
+    #   }
+    # }
     field(:origin, struct(), struct: Properties.Origin)
 
     # URI: https://www.w3.org/ns/activitystreams#instrument
     # Identifies one or more objects used (or to be used) in the completion of an Activity.
     # Domain:	Activity
+    # Example:
+    # {
+    #   "actor": {
+    #     "type": "Person",
+    #     "name": "Sally"
+    #   },
+    #   "object": "http://example.org/foo.mp3",
+    #   "instrument": {
+    #     "type": "Service",
+    #     "name": "Acme Music Service"
+    #   }
+    # }
     field(:instrument, struct(), struct: Properties.Instrument)
   end
 end
